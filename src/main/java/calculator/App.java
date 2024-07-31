018560c292c057202f79004f335634d3f9e18c6d
Robbie <robbie331@>
2024-07-31 13:16:23 +0900

    1 - 2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -2,16 +2,20 @@ package calculator;
 
 import java.util.Scanner;
 
-
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
+        System.out.println("첫번째 숫자를 입력하세요");
         int num = sc.nextInt();
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        System.out.print("두 번째 숫자를 입력하세요: ");
+        System.out.println("두번째 숫자를 입력하세요");
         int num1 = sc.nextInt();
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0);
+
+        System.out.println("operator = " + operator);
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
     }
 }
